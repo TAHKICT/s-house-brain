3195c0e09368fbb586666d10b3c13cfffc192355
TAHKICT <mykh.roma@gmail.com>
2018-05-28 18:07:07 +0300

    fixed web api url

--- src/main/java/com/shouse/restapi/notifires/RestWebApiNotifier.java
+++ src/main/java/com/shouse/restapi/notifires/RestWebApiNotifier.java
@@ -9,7 +9,6 @@ import shouse.core.node.response.Response;
 public class RestWebApiNotifier implements Notifier{
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-
     private RestTemplate restTemplate;
 
     public RestWebApiNotifier(RestTemplate restTemplate){
@@ -19,7 +18,7 @@ public class RestWebApiNotifier implements Notifier{
     @Override
     public void sendResponse(Response response) {
         LOGGER.info("Send response to web api: ".concat(response.toString()));
-        restTemplate.postForEntity("http://localhost:8181/for-core-application/entry-point", response, Response.class);
+        restTemplate.postForEntity("http://localhost:8282/web-rest-api/for-core-application/entry-point", response, Response.class);
     }
 
 }
